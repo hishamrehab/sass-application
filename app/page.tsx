5d20204a390bd0b91c04821e6914e9f45d3387e6
hishamrehab <hishamrehab30@gmail.com>
2026-02-23 22:28:58 +0200

    Adding Linking & Navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
        <h1>Hello from the index page</h1>
+        <Link href="/abc">GO TO ABC</Link>
     </div>
   );
 }
